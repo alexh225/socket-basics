3e8df4d4bb57a4c54a00e51d557f9f7d5da77838
Alex Huber <alexh225@gmx.de>
2016-01-17 19:50:38 +0100

    Add front end form elements for send messages

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,20 @@
             console.log('Connected to the server');
         });
 
-socket.on('message', function(message) {
-    console.log('New message');
-    console.log(message.text);
-});
+        socket.on('message', function (message) {
+            console.log('New message');
+            console.log(message.text);
+        });
+
+        var $msgSubmitBtn = $('#msg-submit');
+
+        $msgSubmitBtn.on('click', function (event) {
+            var $msgText = $('#msg-text');
+
+            socket.emit('message', {
+                text: $msgText.val()
+            });
+
+
+            $msgText.val('');
+        });
--- server.js
+++ server.js
@@ -9,9 +9,16 @@ app.use(express.static(__dirname + '/public'));
 io.on('connection', function (socket) {
     console.log('User connected via socket.io');
 
+    socket.on('message', function(message){
+        console.log('Message ' + message.text);
+        
+        socket.broadcast.emit('message', message);
+        
+    });
+    
     socket.emit('message', {
         text: 'Welcome to the chat application'
-    })
+    });
 
 })
 
